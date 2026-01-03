f1dedb04266ca0c465797a6a146f0c3467127e4f
tobias Madlberger <tobias.madlberger@gmail.com>
2026-01-03 11:30:17 +0100

    fixed dokka & doc builds

--- improved-factions/build.gradle.kts
+++ improved-factions/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("maven-publish")
     id("com.gradleup.shadow") version "9.0.0-beta13"
     id("com.github.ben-manes.versions") version "0.52.0"
-    id("org.jetbrains.dokka") version "2.0.0"
+    id("org.jetbrains.dokka") version "2.1.0"
 }
 
 val versionPropsFile = file("version.properties")
@@ -103,8 +103,10 @@ tasks.withType<KspTask>().configureEach {
     dependsOn(generateBuildConfig)
 }
 
-tasks.register<org.jetbrains.dokka.gradle.DokkaTask>("dokkaDocusaurus") {
-    outputDirectory.set(file("../improved-factions-docs/static/api"))
+dokka {
+    dokkaPublications.html {
+        outputDirectory.set(file("../improved-factions-docs/static/api"))
+    }
     dokkaSourceSets.configureEach {
         skipEmptyPackages.set(true)
         reportUndocumented.set(true)
